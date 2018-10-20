30bf1b16219c57ed00286f5500e118d4e2066cfc
Дмитрий <dmitriy.nezhidenko@gmail.com>
2018-10-20 11:02:48 +0300

    Init project, added gitignore

---
+++ USERS-LIST/USERS-LIST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  USERS-LIST
+//
+//  Created by Дмитрий on 20.10.2018.
+//  Copyright © 2018 Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
